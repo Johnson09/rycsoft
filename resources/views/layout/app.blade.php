86641a177acacafeac291f0366f365bbf00fcc87
Jhon Sotelo <jhonstiven1409@gmail.com>
2020-08-14 09:17:10 -0500

    Config datepicker

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -21,7 +21,7 @@
 
   <!-- Files required Datepicker -->
   <script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
 
   <!-- Sweet Alert -->
