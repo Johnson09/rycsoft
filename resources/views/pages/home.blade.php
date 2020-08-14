55a7a061d0511aac0190db9bc1692cb36d48844e
Jhon Sotelo <jhonstiven1409@gmail.com>
2020-08-13 19:58:24 -0500

    Modify logo width

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,9 @@
     <div class="row justify-content-center text-center">
         <div class="container-fluid text-center">
             <h1 style="color: #2c53c5; margin-top: -0.8em;"><b>RYCSOFT</b></h1>
+            <br>
             <img src="public/images/logopag.png" id="logo">
+            <br>
             <h4>E.S.E HOSPITAL DIVINO NIÑO</h4>
         </div>
     </div>
