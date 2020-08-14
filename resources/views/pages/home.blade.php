ab2d71c345d5ca7972039af3800afce3821ba799
Jhon Sotelo <jhonstiven1409@gmail.com>
2020-08-13 19:53:09 -0500

    Change title name, and put hospital name in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,15 @@
 <div class="container-fluid">
     <div class="row justify-content-center text-center">
         <div class="container-fluid text-center">
-            <h1 style="color: #2c53c5; margin-top: -0.8em;"><b>Seguimiento</b></h1>
-        </div>
-        <div>
+            <h1 style="color: #2c53c5; margin-top: -0.8em;"><b>RYCSOFT</b></h1>
             <img src="public/images/logopag.png" id="logo">
+            <h4>E.S.E HOSPITAL DIVINO NIÑO</h4>
         </div>
     </div>
 </div>
 <style type="text/css">
 	#logo {
-		width: 70%;
+		width: 100%;
 		display: block;
 		margin: auto;
 	}
