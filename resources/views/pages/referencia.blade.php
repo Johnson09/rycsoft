7e5e1779507ab3a382492d1f9a67e213924e0f36
Jhon Sotelo <jhonstiven1409@gmail.com>
2020-08-14 09:09:28 -0500

    Config datepicker

--- resources/views/pages/referencia.blade.php
+++ resources/views/pages/referencia.blade.php
@@ -669,10 +669,6 @@ function actualizar(){
 }
 </style>
 <!-- <script src="https://code.jquery.pro/jquery-3.2.1.js"></script> -->
-  <!-- Files required Datepicker -->
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script> -->
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 	<script type="text/javascript">
 
